76b8caec99f444d7d61fcac8f3d433382490dbfa
SuKi <gcloud0x1@gmail.com>
2025-08-18 19:51:14 +0530

    added support code for oled

--- firmware/config.h
+++ firmware/config.h
@@ -1,24 +1,24 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Pins
 #define REED_PIN 4
 #define LED_PIN 13
 #define SD_CS_PIN 5
 
-// Settings
+#define OLED_ADDRESS 0x3C
+#define OLED_SDA 21
+#define OLED_SCL 22
+
 #define DEBOUNCE_MS 30
 #define TIME_SYNC_RETRIES 10
 #define TIME_SYNC_DELAY 1000
 #define WIFI_TIMEOUT 20000
 
-// Files
 #define LOG_FILE "/doorlog.csv"
 #define STATS_FILE "/stats.json"
 
-// Time
 #define NTP_SERVER "pool.ntp.org"
-#define GMT_OFFSET_SEC 19800  // UTC+5:30 (This is for India)
+#define GMT_OFFSET_SEC 19800   // UTC+5:30 (India)
 #define DAYLIGHT_OFFSET_SEC 0
 
 #endif
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -3,6 +3,7 @@
 #include "timeutils.h"
 #include "sdlogger.h"
 #include "webui.h"
+#include "display.h"
 
 volatile unsigned long doorOpenTime = 0;
 volatile unsigned long lastDuration = 0;
@@ -10,16 +11,19 @@ volatile int lastRawState = HIGH;
 volatile bool lastStableState = false;
 volatile unsigned long lastDebounceTime = 0;
 
-void setup() 
+void setup()
 {
     Serial.begin(115200);
     delay(2000);
-    
+
     Serial.println("\n=== Door Logie ===");
-    
+
     pinMode(REED_PIN, INPUT_PULLUP);
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, LOW);
+
+    Wire.begin(OLED_SDA, OLED_SCL);
+    setupDisplay();
 
     WiFi.begin(WIFI_SSID, WIFI_PASS);
     Serial.print("Connecting to WiFi");
@@ -30,20 +34,20 @@ void setup()
         Serial.print(".");
     }
 
-    if (WiFi.status() == WL_CONNECTED) 
+    if (WiFi.status() == WL_CONNECTED)
     {
         Serial.println("\nWiFi connected! IP: " + WiFi.localIP().toString());
         setupTime();
-    } 
-    else 
+    }
+    else
     {
         Serial.println("\nWiFi connection failed");
     }
 
-    if (!initSD()) 
+    if (!initSD())
     {
         Serial.println("SD card initialization failed!");
-        while (1) 
+        while (1)
         {
             delay(1000);
         }
@@ -54,57 +58,67 @@ void setup()
     lastRawState = digitalRead(REED_PIN);
     currentDoorState = lastRawState == HIGH ? "OPEN" : "CLOSED";
     Serial.println("System ready. Initial state: " + currentDoorState);
+
+    updateDisplay();
 }
 
-void loop() 
+void loop()
 {
     static unsigned long lastStatusBroadcast = 0;
-    
+    static unsigned long lastDisplayUpdate = 0;
+
     int currentReading = digitalRead(REED_PIN);
-    if (currentReading != lastRawState) 
+    if (currentReading != lastRawState)
     {
         lastDebounceTime = millis();
         lastRawState = currentReading;
     }
 
-    if ((millis() - lastDebounceTime) > DEBOUNCE_MS) 
+    if ((millis() - lastDebounceTime) > DEBOUNCE_MS)
     {
         bool currentState = (lastRawState == HIGH);
-        
-        if (currentState != lastStableState) 
+
+        if (currentState != lastStableState)
         {
             lastStableState = currentState;
             digitalWrite(LED_PIN, currentState ? HIGH : LOW);
             currentDoorState = currentState ? "OPEN" : "CLOSED";
-            
-            if (currentState) 
+
+            if (currentState)
             {
                 doorOpenTime = millis();
                 updateDailyStats();
                 Serial.println("Door OPENED at " + getTime());
-            } 
-            else if (doorOpenTime > 0) 
+            }
+            else if (doorOpenTime > 0)
             {
                 lastDuration = (millis() - doorOpenTime) / 1000;
                 lastOpenDuration = lastDuration;
                 logEvent("CLOSED", lastDuration);
                 doorOpenTime = 0;
-                Serial.printf("Door CLOSED after %lu seconds at %s\n", lastDuration, getTime().c_str());
+                Serial.printf("Door CLOSED after %lu seconds at %s\n",
+                              lastDuration, getTime().c_str());
             }
-            
+
             String statusUpdate = currentDoorState + "|" + String(lastOpenDuration);
             webSocket.broadcastTXT(statusUpdate);
+            updateDisplay();
         }
     }
 
-    if (millis() - lastStatusBroadcast >= 1000) 
+    if (millis() - lastStatusBroadcast >= 1000)
     {
         String statusUpdate = currentDoorState + "|" + String(lastOpenDuration);
         webSocket.broadcastTXT(statusUpdate);
         lastStatusBroadcast = millis();
     }
 
+    if (millis() - lastDisplayUpdate >= 1000)
+    {
+        updateDisplay();
+        lastDisplayUpdate = millis();
+    }
+
     handleWeb();
-    
     delay(10);
 }
