cecc0d6202ec137fb6d57e2b4985cd5551994138
SuKi <gcloud0x1@gmail.com>
2025-08-18 22:41:31 +0530

    added neopixel status indicator :   - 5 different indications for 5 different status

---
+++ firmware/neopixel.h
@@ -0,0 +1,82 @@
+#ifndef NEOPIXEL_H
+#define NEOPIXEL_H
+
+#include <Adafruit_NeoPixel.h>
+#include "config.h"
+
+Adafruit_NeoPixel pixels(NEOPIXEL_COUNT, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+// Colors
+#define COLOR_OFF pixels.Color(0, 0, 0)
+// White for booting
+#define COLOR_BOOT pixels.Color(255, 255, 255)
+// Blue for WiFi connecting
+#define COLOR_WIFI_CONN pixels.Color(0, 0, 255)
+// Green for WiFi connected
+#define COLOR_WIFI_OK pixels.Color(0, 255, 0)
+// Yellow for time syncing
+#define COLOR_TIME_SYNC pixels.Color(255, 255, 0)
+// Red for time sync failed
+#define COLOR_TIME_FAIL pixels.Color(255, 0, 0)
+// Red for SD fail
+#define COLOR_SD_FAIL pixels.Color(255, 0, 0)
+// Red for door open
+#define COLOR_DOOR_OPEN pixels.Color(255, 0, 0)
+// Green for door closed
+#define COLOR_DOOR_CLS pixels.Color(0, 255, 0)
+// Red for general error
+#define COLOR_ERROR pixels.Color(255, 0, 0)
+
+void setupNeoPixel()
+{
+    pixels.begin();
+    pixels.clear();
+    pixels.show();
+}
+
+void setStatusColor(uint32_t color, bool blink = false)
+{
+    pixels.clear();
+    if (blink)
+    {
+        pixels.setPixelColor(0, color);
+        pixels.show();
+        delay(500);
+
+        pixels.setPixelColor(0, COLOR_OFF);
+        pixels.show();
+        delay(500);
+    }
+    else
+    {
+        pixels.setPixelColor(0, color);
+        pixels.show();
+    }
+}
+
+void pulseColor(uint32_t color)
+{
+    for (int i = 0; i < 256; i += 5)
+    {
+        pixels.setPixelColor(0, pixels.Color(
+            (color >> 16 & 0xFF) * i / 255,
+            (color >> 8 & 0xFF) * i / 255,
+            (color & 0xFF) * i / 255
+        ));
+        pixels.show();
+        delay(10);
+    }
+
+    for (int i = 255; i >= 0; i -= 5)
+    {
+        pixels.setPixelColor(0, pixels.Color(
+            (color >> 16 & 0xFF) * i / 255,
+            (color >> 8 & 0xFF) * i / 255,
+            (color & 0xFF) * i / 255
+        ));
+        pixels.show();
+        delay(10);
+    }
+}
+
+#endif
